1b963dfde8a21d67b0f1e0e8a3fdeb9875e22593
Mvb1122 <micahbushman.school@gmail.com>
2021-10-05 21:42:20 -0600

    Finish it up.

--- Piggy Bank Lab/src/banks/PiggyBank.java
+++ Piggy Bank Lab/src/banks/PiggyBank.java
@@ -2,22 +2,39 @@ package banks;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class PiggyBank {
+    // Create arrays to hold the values of the coins, and their nouns.
     final static double[] values;
     final static String[][] names;
+
+    // Create an array to hold a bank's coins.
     int[] coins;
 
     static {
-        values = new double[]{0.01, 0.1, 0.25, 0.5, 1};
-        names = new String[][]{{"penny", "pennies"}, {"dime", "dimes"}, {"quarter", "quarters"}, {"half-dollar", "half dollar", "half dollars", "half-dollars"}, {"dollar", "dollars"}};
+        // Statically initialize the arrays for nouns and coin values.
+        /*
+         The way I have this setup, each String[] in `names` is equivalent to an index in values.
+         Then, in each String[] on names, I have the singular name before the plural name.
+         In the case of the half-dollar, I've allowed for four different nouns, all of which work.
+        */
+        values = new double[]{0.01, 0.05, 0.1, 0.25, 0.5, 1};
+        names = new String[][]{{"penny", "pennies"}, {"nickel", "nickels"}, {"dime", "dimes"}, {"quarter", "quarters"}, {"half-dollar", "half dollars", "half dollar", "half-dollars"}, {"dollar", "dollars"}};
     }
 
+    /**
+     * Create an empty bank.
+     */
     public PiggyBank() {
         coins = new int[names.length];
     }
 
+    /**
+     * Creates a bank from an array of coins.
+     * @param coins An array of coins in the following order: Pennies, Nickels, Dimes, Quarters, Half-Dollars, and dollars.
+     */
     public PiggyBank(int[] coins) {
         if (coins.length != names.length) {
             System.out.println("Your input coins Array is of an incorrect length!");
@@ -27,25 +44,69 @@ public class PiggyBank {
         System.arraycopy(coins, 0, this.coins, 0, names.length);
     }
 
+    /**
+     * Adds a specified number of coins to the bank.
+     * @param numCoins The number of coins to add.
+     * @param coin A string which decides the type of coin, either Pennies, Nickels, Dimes, Quarters, Half-Dollars, or dollars.
+     */
     public void add(int numCoins, String coin) {
-        for (int i = 0; i < coins.length; i++) {
-            if (coin.equals(names[i])) coins[i] += numCoins;
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names[i].length; i++) {
+                if (coin.equals(names[i][j])) coins[i] += numCoins;
+                if (this.coins[i] < 0) this.coins[i] = 0;
+            }
         }
     }
 
+    /**
+     * Subtracts a specified number of coins from the bank.
+     * @param numCoins The number of coins to remove.
+     * @param coin A string which decides the type of coin, either Pennies, Nickels, Dimes, Quarters, Half-Dollars, or dollars.
+     */
     public void subtract(int numCoins, String coin) {
         this.add(-numCoins, coin);
     }
 
-    public int getCoin(String coin) {
+    /**
+     * Subtracts any number of coins directly from the bank.
+     * @param coins An array holding the values of coins to remove in the following order: Pennies, Nickels, Dimes, Quarters, Half-Dollars, and dollars.
+     */
+    public void subtract(int[] coins) {
         for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j < names[i].length; i++) {
-                if (coin.equals(names[i][j])) return coins[i];
+            coins[i] = -coins[i];
+        }
+        this.add(coins);
+    }
+
+    /**
+     * Adds any number of coins directly from the bank.
+     * @param coins An array holding the values of coins to add in the following order: Pennies, Nickels, Dimes, Quarters, Half-Dollars, and dollars.
+     */
+    public void add(int[] coins) {
+        for (int i = 0; i < this.coins.length; i++) {
+            this.coins[i] += coins[i];
+            if (this.coins[i] < 0) this.coins[i] = 0;
+        }
+    }
+
+    /**
+     * Gets the number of a coin in a bank.
+     * @param coin The coin type to remove, any of the following: Pennies, Nickels, Dimes, Quarters, Half-Dollars, and dollars.
+     * @return The number of the specified coin in the bank.
+     */
+    public int getCoin(String coin) {
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names[i].length; j++) {
+                if (coin.toLowerCase(Locale.ROOT).equals(names[i][j])) return coins[i];
             }
         }
         return 0;
     }
 
+    /**
+     * Gets the value, in dollars, of the bank.
+     * @return The value in dollars of the bank's contents.
+     */
     public double getValue() {
         double value = 0;
         for (int i = 0; i < coins.length; i++) {
@@ -54,16 +115,20 @@ public class PiggyBank {
         return value;
     }
 
+    /**
+     * Generates a bank from a string of "natural" language.
+     * @param s A string which contains a set of coins, see the <a href="https://github.com/Mvb1122/Labs/blob/main/Piggy%20Bank%20Lab/Examples.md">examples</a> for examples.
+     */
     public PiggyBank(String s) {
         this(parse(s));
     }
 
     public static int[] parse(String s) {
-        // Remove any commas from the input.
+        // Remove any commas or new-lines from the input.
         {
             StringBuilder d = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) != ',') d.append(s.charAt(i));
+                if (s.charAt(i) != ',' && s.charAt(i) != '\n') d.append(s.charAt(i));
             }
             s = d.toString();
         }
@@ -88,6 +153,7 @@ public class PiggyBank {
         // Loop through every third space.
             // Create an Array to hold the outputted coins.
         int[] coins = new int[names.length];
+
         for (int i = 0; i < spaceIndexes.size(); i += 2) {
             try {
                 // Make a substring from the index of the previous space (or start) to the next space (or end).
@@ -107,7 +173,7 @@ public class PiggyBank {
                 // Increase the coin in the array by the value of the numCoins.
                 coins[getCoinIndex(coin.toLowerCase(Locale.ROOT).trim())] += numCoins;
 
-                System.out.println(parts);
+                // System.out.println(parts);
             } catch (IndexOutOfBoundsException e) {
                 // See above comments.
                 String parts = s.substring(spaceIndexes.get(i)).trim();
@@ -115,7 +181,6 @@ public class PiggyBank {
                 int numCoins = Integer.parseInt(parts.substring(0, firstSpaceIndex));
                 String coin = parts.substring(firstSpaceIndex);
                 coins[getCoinIndex(coin.toLowerCase(Locale.ROOT).trim())] += numCoins;
-                System.out.println(parts);
                 break;
             }
         }
@@ -146,5 +211,55 @@ public class PiggyBank {
                 if (coin.equals(names[i][j])) coins[i] = value;
             }
         }
+    }
+
+    public void set(int[] input) {
+        if (input.length != names.length) System.out.println("Your input array is not the correct length! (" + input.length + " != " + names.length + ")");
+        else this.coins = input;
+    }
+
+    public String toString() {
+        // Find the last filled coin.
+        int lastFilledCoin = 0;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            if (coins[i] != 0) {
+                lastFilledCoin = i;
+                break;
+            }
+        }
+        
+        // Check if there's more than 1 filled slot and if the bank is empty.
+        boolean moreThanOneFilledSlot = false;
+        boolean empty = false;
+        {
+            int filledSlots = 0;
+            for (int i = 0; i < coins.length; i++) {
+                if (coins[i] != 0) filledSlots++;
+            }
+            if (filledSlots > 2) moreThanOneFilledSlot = true;
+            if (filledSlots == 0) empty = true;
+        }
+
+        String output = "This piggy bank has ";
+        if (!empty) for (int i = 0; i < names.length; i++) {
+            if (coins[i] > 0) {
+                // If this is the last filled coin, and there are more than 1 filled slots, put an "and".
+                if (lastFilledCoin == i && moreThanOneFilledSlot) output += " and ";
+
+                output += coins[i] + " ";
+
+                // Add the plural form of the name if it's greater than 1.
+                if (coins[i] == 1) output += names[i][0];
+                else output += names[i][1];
+
+                // If this is the last coin, put a `.`
+                if (lastFilledCoin == i) output += ". ";
+                else output += ", ";
+            }
+        }
+        else output += "nothing. ";
+
+        output += "This gives it a value of $" + getValue() + ".";
+        return output; // "This piggy bank has " + getCoin("pennies") + " pennies, " + getCoin("nickels") + " nickels, " + getCoin("dimes") + " dimes, and " + getCoin("quarters") + " quarters. It has a value of $" + getValue() + ".";
     }
 }
--- Piggy Bank Lab/src/com/company/Main.java
+++ Piggy Bank Lab/src/com/company/Main.java
@@ -2,32 +2,155 @@ package com.company;
 
 import banks.PiggyBank;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] coins = PiggyBank.parse("26 Quarters, 35 Pennies, 29 Dimes");
-
+        // Create a bank for the program to use.
+            // Note, I had to make the piggy-bank atomic, since IntelliJ was complaining about access from other threads. (on a button's click.)
+        AtomicReference<PiggyBank> p = new AtomicReference<>(new PiggyBank("26 Quarters, 35 Pennies, 29 Dimes"));
         JFrame frame = new JFrame("Piggy Bank Lab");
-        frame.setSize(300, 300);
-        frame.setLocation(5, 5);
+        // Set the window's layout to `null` so I can use manual positioning.
+        frame.setLayout(null);
+
+        // Make the program close when the X button on the window is hit.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label = new JLabel(intArrayToString(coins), SwingConstants.CENTER);
+        // Declare layout configuration variables.
+        int maxX, maxY, buttonHeight, buttonWidth;
+
+        // maxX is how wide the window is.
+        maxX = 600;
+
+        // maxY is how tall the window is.
+        maxY = 300;
+
+        // buttonHeight is how tall the buttons at the bottom of the screen are. I chose 50px, since that's what I thought looked the best.
+        buttonHeight = 50;
+
+        // buttonWidth is how wide each button is, and since I only have 3 buttons, I can just divide the width of the window by 3 in order to make 3 equal sections which fit on the window perfectly.
+        buttonWidth = maxX / 3;
+
+        // This sets the window's start location, which is 5 pixels from the top right corner of your monitor, both sideways and downwards.
+        frame.setLocation(5, 5);
+
+        // This creates the label which displays the bank's value at the top of the screen.
+        JLabel label = new JLabel(p.toString(), SwingConstants.CENTER);
+        // Since I want the label to fit to the width of the window, I tell it to be as wide as the window itself.
+        // Then, since I want it to match the height of the buttons, for horizontal symmetry, I tell it to be as tall as the buttons themselves.
+        label.setBounds(0, 0, maxX, buttonHeight);
+
+        // Create a text field where the user can enter the contents of their piggy bank.
+        JTextField input = new JTextField("26 Quarters, 35 Pennies, 29 Dimes", 1);
+
+        /*
+         I want the text field to run down from the bottom of the label as far as it can, while leaving space for the buttons, as well as being as wide as the window itself,
+         so I tell it to be as wide as the screen, and I set the y-start to the button's height, since that's what the label is using as its height.
+         Then, since I want it to run as far as possible, I tell it to be as tall as the max height, minus twice the button's height.
+         The reason I used **twice** and not only once here is because we already have one button-sized object, the label, at the top of the screen,
+         and since I don't want the text area to run off the screen, I had to make enough space for the buttons at the bottom, which are also button sized, thereby requiring **twice** the space of one button.
+        */
+        input.setBounds(0, buttonHeight, maxX, maxY-(2 * buttonHeight));
+
+        // Create a button which submits the input text to the Piggy-bank's constructor.
+        JButton setButton = new JButton("Set");
+
+        /*
+         Since I want the button to start at the edge of the screen, without going off, I tell it to start at the maxX, minus the width of one button,
+         which places it right up against the edge of the window. Next, I place it at the bottom of the window, placed up a height of a button, which
+         the button fills in, thereby making it placed perfectly in the corner of the window.
+        */
+        setButton.setBounds(maxX-buttonWidth, maxY-buttonHeight, buttonWidth, buttonHeight);
+
+        /*
+         Now, I create something called an `Action Listener`, which I apply to that button. It basically allows code to be run whenever something happens,
+         which in this case, is a click on the button.
+        */
+        setButton.addActionListener(e -> {
+            // Here, I'm using try-catch statements, which basically keep the program from crashing due to a certain error, which in this case, is user-based, so we account for
+            // fools using this program by preventing any possible errors from crashing the program.
+            try {
+                try {
+                    // First, I got the input from the text box and then used the parse() method on the PiggyBank class to turn it into the proper array of values that the normal constructor uses.
+                    int[] displayedCoins = PiggyBank.parse(input.getText());
+                    // Then, I get the PiggyBank and set the value of it to the user's input.
+                    p.get().set(displayedCoins);
+                    // Next and finally, I update the display to hold the latest information on the PiggyBank.
+                    label.setText(p.get().toString());
+                } catch (NumberFormatException N) {
+                    // Do nothing, since this means that the user's input is malformed, and they probably accidentally clicked the button.
+                }
+            } catch (StringIndexOutOfBoundsException S) {
+                // See above comment.
+            }
+        });
+
+        // ~~~~ The code below here is basically copy and paste, so skip to line 139 ~~~~
+
+        // Create a button which adds the specified coins.
+        JButton addButton = new JButton("Add");
+
+        // I'll save you the reading, but this basically places the button in the bottom-center of the screen.
+        addButton.setBounds(maxX-(2 * buttonWidth), maxY-buttonHeight, buttonWidth, buttonHeight);
+
+        addButton.addActionListener(e -> {
+            try {
+                try {
+                    // Gets the user's input in the proper form.
+                    int[] displayedCoins = PiggyBank.parse(input.getText());
+                    // Adds the user's input to the bank.
+                    p.get().add(displayedCoins);
+                    // Updates the label.
+                    label.setText(p.get().toString());
+                } catch (NumberFormatException N) {
+                    // Do nothing, since this means that the user's input is malformed, and they probably accidentally clicked the button.
+                }
+            } catch (StringIndexOutOfBoundsException S) {
+                // See above comment.
+            }
+        });
+
+        // Create a button which subtracts the specified coins.
+        JButton subtractButton = new JButton("Subtract");
+
+        // This is the third time, so this code places the button in the bottom left of the screen.
+        subtractButton.setBounds(maxX-(3 * buttonWidth), maxY-buttonHeight, buttonWidth, buttonHeight);
+        // Make the button take the appropriate actions when it's clicked.
+        subtractButton.addActionListener(e -> {
+            try {
+                try {
+                    // Get
+                    int[] displayedCoins = PiggyBank.parse(input.getText());
+                    // Set
+                    p.get().subtract(displayedCoins);
+                    // Display
+                    label.setText(p.get().toString());
+                } catch (NumberFormatException N) {
+                    // Do nothing, since this means that the user's input is malformed, and they probably accidentally clicked the button.
+                }
+            } catch (StringIndexOutOfBoundsException S) {
+                // See above comment.
+            }
+        });
+
+        // Now, to wrap things up, we just add all the buttons, the label, and the display to the window,
+        frame.add(subtractButton);
+        frame.add(addButton);
+        frame.add(setButton);
         frame.add(label);
+        frame.add(input);
 
-        frame.show();
-    }
+        /*
+         Set the window's size to match the variables I've been referencing so far.
+         Additionally, I set it just a little taller than the values I've used, just to add some white-space at the bottom
+         of the window, because I thought it looked better than having it perfectly match up.
+        */
+        frame.setSize(maxX, maxY + 50);
 
-    private static String intArrayToString(int[] v) {
-        String s = "[";
-        for (int i = 0; i < v.length; i++) {
-            if (i == 0) s += v[i]; else s += ", " + v[i];
-        }
-        s += "]";
-        return s;
+        // And finally, I set the window to display on the screen.
+        frame.setVisible(true);
     }
 }
